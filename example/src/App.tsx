a61b1fa85f2752e8bda60f11955b4bfca9d0d6f7
Mauricio Blum <mauricio.blum@itsector.pt>
2020-08-22 17:35:50 +0100

    fix(svg): converted html svg to component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,10 @@
 import * as React from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
-import { Card } from 'react-native-superdda-iupay';
 
 export default function App() {
   return (
     <ScrollView style={styles.scroll}>
-      <View style={styles.container}>
-        <Card
-          type="netflix"
-          dueDate={new Date()}
-          isDue
-          cnpj="32.773.0001/87"
-          text="Tipo de Plano: Premium ULTRA HD"
-          value={49.9}
-          containerStyle={styles.card}
-        />
-
-        <Card
-          type="lightBill"
-          lightBillFlagStatus="yellow"
-          dueDate={new Date()}
-          cnpj="131.344.523/0001-33"
-          text="Bandeira Amarela"
-          value={90.12}
-          isPaid
-        />
-      </View>
+      <View style={styles.container} />
     </ScrollView>
   );
 }
